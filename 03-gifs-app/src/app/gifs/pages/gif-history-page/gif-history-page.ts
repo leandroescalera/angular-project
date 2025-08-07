c1416aff0b93f72fee02cbb6ee8a0e67f7c9a11c
Leandro Escalera <leandro_aslom619@hotmail.com>
2025-08-06 22:15:14 -0400

    feat(gif-history-page): DISPONIBILIZACION DE HISTORY
    Se disponibilizo el componente para obtener el historial en el menu y pueda mostrar los gifs.
    
    Closes #89 - Mostrar historial de busqueda.

--- 03-gifs-app/src/app/gifs/pages/gif-history-page/gif-history-page.ts
+++ 03-gifs-app/src/app/gifs/pages/gif-history-page/gif-history-page.ts
@@ -1,20 +1,28 @@
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
+import { GifService } from '@gifs/services/gifs.service';
+import { ListBodyComponent } from "@gifs/components/list-body/list-body";
 
 @Component({
   selector: 'app-gif-history-page',
-  imports: [],
+  imports: [ListBodyComponent],
   templateUrl: './gif-history-page.html',
   styleUrl: './gif-history-page.css'
 })
 export default class GifHistoryPage {
 
+  gifsService = inject(GifService);
+
   query = toSignal(
     inject(ActivatedRoute).params.pipe(
-      map(params => params['query']))
+      map((params) => params['query']))
   );
+
+  gifsByQuery = computed(() => {
+    return this.gifsService.searchHistory()[this.query() ?? ''];
+  })
 
 }
 
--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,11 @@
-import { computed, inject, Injectable, Query, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
 import { Gif } from '@gifs/interfaces/gif.interface';
 import type { GiphyResponse } from '@gifs/interfaces/giphy.interfaces';
 import { GifMapper } from '@gifs/mapper/gif.mapper';
-import { map, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
@@ -37,7 +37,7 @@ export class GifService {
     })
   }
 
-  searchGifs(query: string) {
+  searchGifs(query: string): Observable<Gif[]> {
     return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
       params: {
         api_key: environment.gyphyApiKey,
@@ -57,6 +57,9 @@ export class GifService {
         }));
       })
     );
+  }
 
+  getHistoryGifs(query: string): Gif[] {
+    return this.searchHistory()[query] ?? [];
   }
 }
