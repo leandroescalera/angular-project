b8c7c7b2987e0d419f908ca5e85690fd499a2e47
Leandro Escalera <leandro_aslom619@hotmail.com>
2025-09-09 23:23:34 -0400

    feat(top-menu): CREACION DE ARTEFACTO
    Se creo el artefacto para la creacion del menu princiapal a las distintas rutas.

--- 04-country-app/src/app/app.ts
+++ 04-country-app/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./share/components/footer/footer";
+import { TopMenuComponent } from './country/components/top-menu/top-menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, TopMenuComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  
+
 })
 export class App {
   protected title = 'country-app';
