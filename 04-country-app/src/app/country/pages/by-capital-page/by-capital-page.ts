1d129688339b36c1b60296e17692dea353fb6ef3
Leandro Escalera <leandro_aslom619@hotmail.com>
2025-11-27 00:21:38 -0400

    feat(ByCapitalPageComponent): ACTUALIZACION DE ARCHIVO
    Se actualizo la funcion depurando codigo.
    
    Closes #123 Decimal Pipe

--- 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.ts
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,8 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input";
 import { CountryListComponent } from "../../components/country-list/country-list";
 import { CountryService } from '../../services/country.service';
-import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -11,28 +11,16 @@ import { Country } from '../../interfaces/country.interface';
   styleUrl: './by-capital-page.css'
 })
 export class ByCapitalPageComponent {
-
   countryService = inject(CountryService);
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
-
-
-  onSearch(query: string) {
-    if (this.isLoading()) return;
-
-    this.isLoading.set(true);
-    this.isError.set(null);
-
-    this.countryService.searchByCapital(query)
-      .subscribe({
-        next: (countries) => {
-          this.isLoading.set(false);
-          this.countries.set(countries);
-        },error: (err) => {
-          this.isLoading.set(false);
-          this.countries.set([]);
-          this.isError.(`No se encontro un pais con esa capital ${query}´);
-      });
-  }
+  query = signal('');
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request }) => {
+      if (this.query()) return [];
+      return await firstValueFrom(
+        this.countryService.searchByCapital(request.query)
+      );
+    },
+  });
 }
+
