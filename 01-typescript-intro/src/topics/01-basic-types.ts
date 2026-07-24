786907220533b172a7950a82622e2c0ec4e54657
leandro.escalera <marcelo.escalera.inf@gmail.com>
2026-07-23 22:47:33 -0400

    feat(01-basic-types.ts): TIPOS BASICOS Y CONCEPTOS GENERALES
    Ejmplo de importacion
    
    Closes #12

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,9 @@
+const name: string = 'John Doe'; 
+let hpPoints: number | 'FULL' = 100; 
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({ name, hpPoints, isAlive });
+
+export {};
