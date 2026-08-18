7ced78fb762efd331e26b95ff8f209961eac1a00
leandro.escalera <marcelo.escalera.inf@gmail.com>
2026-08-17 23:23:05 -0400

    feat(05-basic-destructuring.ts): DESESTRUCTURACION DE OBJETOS
    Creacion de objetos y destructuracion para obterner las variables de un objto.
    
    Closes #16

---
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -0,0 +1,31 @@
+interface AudioPlayer {
+    audioVolume: number;
+    songDuration: number;
+    song: string;
+    details: Details;
+}
+
+interface Details {
+    author: string;
+    year: number;
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolume: 90,
+    songDuration: 36,
+    song: "Mess",
+    details: {
+        author: "Ed Sheeran",
+        year: 2020
+    }
+}
+
+const { song: anotherSong, songDuration: duration, details } = audioPlayer;
+
+const { author } = details;
+
+console.log('Song: ', anotherSong);
+console.log('Song Duration: ', duration);
+console.log('Author: ', author);
+
+export { };
