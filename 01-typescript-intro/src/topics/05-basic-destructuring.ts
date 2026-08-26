4a61519287250f52d1a4f4c84f68eb400d4ccc60
leandro.escalera <marcelo.escalera.inf@gmail.com>
2026-08-25 23:52:49 -0400

    feat(05-basic-destructuring.ts): DESESTRUCTURACION DE OBJETOS
    Creacion de objetos y destructuracion para obterner las variables de un objto.

--- 01-typescript-intro/src/topics/05-basic-destructuring.ts
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -24,8 +24,18 @@ const { song: anotherSong, songDuration: duration, details } = audioPlayer;
 
 const { author } = details;
 
-console.log('Song: ', anotherSong);
-console.log('Song Duration: ', duration);
-console.log('Author: ', author);
+// console.log('Song: ', anotherSong);
+// console.log('Song Duration: ', duration);
+// console.log('Author: ', author);
+
+const [personaje1, personaje2, personaje3]: string[] = ['Goku', 'Vegeta', 'Trunks'];
+
+console.log('Personaje 1: ', personaje1 || "Personaje no encontrado");
+console.log('Personaje 2: ', personaje2 || "Personaje no encontrado");
+console.log('Personaje 3: ', personaje3 || "Personaje no encontrado");
+
+console.error('Personaje 4: ', dbz[3] || "Personaje no encontrado");
+
+console.table(dbz);
 
 export { };
